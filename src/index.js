93bff78d5ed943b3398a0ab513e61e92b9d00a7e
HyeonuPark <nemo1275@gmail.com>
2016-05-29 16:17:50 +0900

    add missing index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+export {resolve} from './resolve'
+export {range} from './range'
+export {product} from './product'
+
+export {indexed} from './indexed'
+export {toArray} from './toArray'
+
+export {every} from './every'
+export {filter} from './filter'
+
+export {map} from './map'
+export {some} from './some'
